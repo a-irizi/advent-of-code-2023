d16f928dca56bf79c3a82c56bd2be3f7b743080b
a-irizi <irizi.anass.00@gmail.com>
2024-04-30 16:28:44 +0100

    solved day1 part 1

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let file_name = "input.txt";
+    let calibration: u64 = std::fs::read_to_string(file_name)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let digit_chars = line
+                .chars()
+                .filter(|char| char.is_digit(10))
+                .collect::<Vec<_>>();
+
+            let (first_digit, last_digit) = if digit_chars.len() > 1 {
+                let last_element_index = digit_chars.len() - 1;
+                (digit_chars[0], digit_chars[last_element_index])
+            } else {
+                (digit_chars[0], digit_chars[0])
+            };
+
+            format!("{}{}", first_digit, last_digit)
+                .parse::<u64>()
+                .unwrap()
+        })
+        .sum();
+
+    println!("Calibration value is {calibration}");
 }
