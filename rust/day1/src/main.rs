dad97c94c99904844f710a8c5ecc48f8849226cb
a-irizi <irizi.anass.00@gmail.com>
2024-09-13 21:55:19 +0100

    solved day 1 part 2 version 2

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,15 +1,7 @@
-trait ICalibrationParser: Iterator {
-    fn parse_digits(self) -> CalibrationParser<Self>
-    where
-        Self: Sized;
-}
-
-pub struct CalibrationParser<T>
-where
-    T: Iterator,
-{
-    inner: T,
-}
+const DIGITS: [&str; 18] = [
+    "1", "one", "2", "two", "3", "three", "4", "four", "5", "five", "6", "six", "7", "seven", "8",
+    "eight", "9", "nine",
+];
 
 pub struct DigitParser<'a> {
     initial: &'a str,
@@ -20,96 +12,50 @@ impl<'a> DigitParser<'a> {
         Self { initial }
     }
 
-    pub fn first_digit(&self) -> Option<char> {
+    pub fn first_digit(&self) -> Option<u64> {
         let mut unparsed;
         for current in 0..self.initial.len() {
             unparsed = &self.initial[current..];
-            if unparsed.starts_with(&['1', '2', '3', '4', '5', '6', '7', '8', '9']) {
-                return unparsed.chars().next();
-            }
-
-            if unparsed.starts_with("one") {
-                return Some('1');
-            }
-
-            if unparsed.starts_with("two") {
-                return Some('2');
-            }
-
-            if unparsed.starts_with("three") {
-                return Some('3');
-            }
-
-            if unparsed.starts_with("four") {
-                return Some('4');
-            }
-
-            if unparsed.starts_with("five") {
-                return Some('5');
-            }
-
-            if unparsed.starts_with("six") {
-                return Some('6');
-            }
-
-            if unparsed.starts_with("seven") {
-                return Some('7');
-            }
-
-            if unparsed.starts_with("eight") {
-                return Some('8');
-            }
-
-            if unparsed.starts_with("nine") {
-                return Some('9');
+            for (index, digit) in DIGITS.into_iter().enumerate() {
+                if unparsed.starts_with(digit) {
+                    match index {
+                        0 | 1 => return Some(1),
+                        2 | 3 => return Some(2),
+                        4 | 5 => return Some(3),
+                        6 | 7 => return Some(4),
+                        8 | 9 => return Some(5),
+                        10 | 11 => return Some(6),
+                        12 | 13 => return Some(7),
+                        14 | 15 => return Some(8),
+                        16 | 17 => return Some(9),
+                        _ => continue,
+                    };
+                }
             }
         }
 
         return None;
     }
 
-    pub fn last_digit(&self) -> Option<char> {
+    pub fn last_digit(&self) -> Option<u64> {
         let mut unparsed;
         for current in (0..self.initial.len()).rev() {
             unparsed = &self.initial[current..];
-            if unparsed.starts_with(&['1', '2', '3', '4', '5', '6', '7', '8', '9']) {
-                return unparsed.chars().next();
-            }
-
-            if unparsed.starts_with("one") {
-                return Some('1');
-            }
-
-            if unparsed.starts_with("two") {
-                return Some('2');
-            }
-
-            if unparsed.starts_with("three") {
-                return Some('3');
-            }
-
-            if unparsed.starts_with("four") {
-                return Some('4');
-            }
-
-            if unparsed.starts_with("five") {
-                return Some('5');
-            }
-
-            if unparsed.starts_with("six") {
-                return Some('6');
-            }
-
-            if unparsed.starts_with("seven") {
-                return Some('7');
-            }
-
-            if unparsed.starts_with("eight") {
-                return Some('8');
-            }
-
-            if unparsed.starts_with("nine") {
-                return Some('9');
+            for (index, digit) in DIGITS.into_iter().enumerate() {
+                if unparsed.starts_with(digit) {
+                    match index {
+                        0 | 1 => return Some(1),
+                        2 | 3 => return Some(2),
+                        4 | 5 => return Some(3),
+                        6 | 7 => return Some(4),
+                        8 | 9 => return Some(5),
+                        10 | 11 => return Some(6),
+                        12 | 13 => return Some(7),
+                        14 | 15 => return Some(8),
+                        16 | 17 => return Some(9),
+                        _ => continue,
+                    };
+                }
             }
         }
 
@@ -131,7 +77,7 @@ mod test {
         let first_digit = digit_parser.first_digit();
 
         // then
-        assert_eq!(first_digit, Some('1'));
+        assert_eq!(first_digit, Some(1));
     }
 
     #[test]
@@ -157,16 +103,16 @@ mod test {
         let first_digit = digit_parser.first_digit();
 
         // then
-        assert_eq!(first_digit, Some('9'));
+        assert_eq!(first_digit, Some(9));
     }
 
     #[test]
     pub fn first_digit_return_valid_resut_for_numeric_digit() {
         // given
         let sample = [
-            ("pqrs7tsixteen", '7', "in the middle of string"),
-            ("2pqrstsixteen", '2', "in the start of string"),
-            ("pqrstsiixteen4", '4', "in the end of string"),
+            ("pqrs7tsixteen", 7, "in the middle of string"),
+            ("2pqrstsixteen", 2, "in the start of string"),
+            ("pqrstsiixteen4", 4, "in the end of string"),
         ];
         for (initial, expected, position) in sample {
             let digit_parser = DigitParser::new(initial);
@@ -195,7 +141,7 @@ mod test {
         let last_digit = digit_parser.last_digit();
 
         // then
-        assert_eq!(last_digit, Some('3'));
+        assert_eq!(last_digit, Some(3));
     }
 
     #[test]
@@ -221,16 +167,16 @@ mod test {
         let first_digit = digit_parser.last_digit();
 
         // then
-        assert_eq!(first_digit, Some('7'));
+        assert_eq!(first_digit, Some(7));
     }
 
     #[test]
     pub fn last_digit_return_valid_resut_for_numeric_digit() {
         // given
         let sample = [
-            ("pqrs7tsixteen", '6', "in the middle of string"),
-            ("4pqrstsiixteen", '4', "in the start of string"),
-            ("pqrstsixteen2", '2', "in the end of string"),
+            ("pqrs7tsixteen", 6, "in the middle of string"),
+            ("4pqrstsiixteen", 4, "in the start of string"),
+            ("pqrstsixteen2", 2, "in the end of string"),
         ];
         for (initial, expected, position) in sample {
             let digit_parser = DigitParser::new(initial);
@@ -250,18 +196,32 @@ mod test {
     }
 }
 
+trait ICalibrationParser: Iterator {
+    fn parse_digits(self) -> CalibrationParser<Self>
+    where
+        Self: Sized;
+}
+
+pub struct CalibrationParser<T>
+where
+    T: Iterator,
+{
+    inner: T,
+}
+
 impl<'a, T> Iterator for CalibrationParser<T>
 where
     T: Iterator<Item = &'a str>,
 {
-    type Item = u32;
+    type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
         while let Some(initial) = self.inner.next() {
             let digit_parser = DigitParser::new(initial);
             let first = digit_parser.first_digit().unwrap();
             let last = digit_parser.last_digit().unwrap();
-            return format!("{}{}", first, last).parse().ok();
+
+            return Some(first * 10 + last);
         }
         None
     }
@@ -279,10 +239,14 @@ where
 
 fn main() {
     let file_name = "input.txt";
-    let calibration: u32 = std::fs::read_to_string(file_name)
+    let calibration: u64 = std::fs::read_to_string(file_name)
         .unwrap()
         .lines()
         .parse_digits()
+        .map(|d| {
+            println!("{}", d);
+            d
+        })
         .sum();
 
     println!("Calibration value is {calibration}");
